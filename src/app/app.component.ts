dab9b823fa85a2d15a8799a0a408e34e6893ec9a
Yblaster <59924957+Yblaster@users.noreply.github.com>
2020-09-01 18:48:55 +0200

    add an observer to show how long you are on the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AppareilService } from './service/appareil.service';
+import { Observable } from 'rxjs-compat/observable';
+import  'rxjs-compat/add/observable/interval';
 
 @Component({
   selector: 'app-root',
@@ -13,6 +15,7 @@ export class AppComponent implements OnInit {
 
   
   appareils: any[];
+  secondes: number;
   
   lastUpdate = new Promise((resolve, reject) => {
 		const date = new Date();
@@ -30,6 +33,21 @@ export class AppComponent implements OnInit {
 
   
   ngOnInit(){
+    const counter = Observable.interval(1000);
+
+    counter.subscribe(
+      (value) => {
+        this.secondes = value;
+      },
+      (error) => { 
+        console.log('un erreur est survenue : ' + error );
+      },
+      () => {
+        console.log('Observable compete!');
+      }
+
+
+    )
   }
   
 
